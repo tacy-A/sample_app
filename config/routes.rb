3f666bf6ebabe475e5efd04e805febe27d55d42a
NeLson <w_m326inwarota@yahoo.co.jp>
2017-12-06 16:35:16 +0000

    Display a user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 SampleApp::Application.routes.draw do
-  devise_for :users
   root 'static_pages#home' #下と同じ意味
+  
+  resources :users, only: [:show]
+  devise_for :users
   # match '/',        to: 'static_pages#home',    via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
