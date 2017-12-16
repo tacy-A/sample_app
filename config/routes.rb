f9b9c1906b54b50790c85b0e2a059494aeefda3d
NeLson <w_m326inwarota@yahoo.co.jp>
2017-12-16 07:57:20 +0000

    finish follow actions

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,14 @@ SampleApp::Application.routes.draw do
     :registrations => "registrations"
   }
   
-  resources :users, only: [:show, :index, :destroy]
-  resources :microposts, only: [:create, :destroy]
+  resources :users, only: [:show, :index, :destroy] do
+    member do
+      get :following, :followers
+    end
+  end
+  
+  resources :microposts,    only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   root 'static_pages#home' #下と同じ意味
   # match '/',        to: 'static_pages#home',    via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
