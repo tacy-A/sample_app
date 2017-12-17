779cd9010fd94ade25ffb6ceff47e73e82e0611f
NeLson <w_m326inwarota@yahoo.co.jp>
2017-12-17 05:39:31 +0000

    finish user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,4 +28,8 @@ class User < ActiveRecord::Base
   def unfollow!(other_user)
     relationships.find_by(followed_id: other_user.id).destroy
   end
+  
+  def feed
+    Micropost.from_users_followed_by(self)
+  end
 end
