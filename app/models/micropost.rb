1af7d90166e5f75d4fe579fbe6d24eca531facb9
NeLson <w_m326inwarota@yahoo.co.jp>
2017-12-23 22:37:28 +0000

    image size change

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,8 +1,9 @@
 class Micropost < ActiveRecord::Base
   belongs_to :user
-  has_attached_file :image, styles: { medium: "200x150>", thumb: "50x50>" }
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "50x50>" }
   default_scope -> { order('created_at DESC') }
   validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png image/gif)
+  validates_attachment_size :image, :less_than=>1.megabyte
   validates :content, presence: true, length: { maximum: 140 }
   validates :user_id, presence: true
   
